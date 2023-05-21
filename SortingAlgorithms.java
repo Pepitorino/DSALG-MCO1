104a95d1fc02ecd01f6b589a21c8074ae8513357
Pepitorino <carlos.ambrosio2007@gmail.com>
2023-05-21 10:09:45 +0800

    Added selection and insertion sort

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -12,12 +12,36 @@ public class SortingAlgorithms {
 
     public void insertionSort(Record[] arr, int n) {
         // TODO: Implement this sorting algorithm here.
-
+	for (int i=1;i<n;i++)
+	{
+		Record temp = arr[i];
+		int j = i;
+		while((j>0)&&(array[j-1].getIdNumber>temp.getIdNumber))
+		{
+			array[j]=array[j-1];
+			j--;
+		}
+		array[j]=temp;
+	}
     }
 
     public void selectionSort(Record[] arr, int n) {
         // TODO: Implement this sorting algorithm here.
-
+	for (int i=0;i<n;i++)
+	{
+		int key=i;
+		Record temp;
+		for (int j=i;j<n;j++)
+		{
+			if (arr[key].getIdNumber<arr[j].getIdNumber)
+			{
+				key=j;
+			}
+		}
+		temp=arr[key];
+		arr[key]=arr[i];
+		arr[i]=temp;
+	}
     }
 
     public void mergeSort(Record[] arr, int p, int r) {
