be66b7ab99808df49048bc75194f4ef1adf38d1e
Pepitorino <carlos.ambrosio2007@gmail.com>
2023-06-04 07:09:38 +0800

    Sorting Algs

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -1,58 +1,111 @@
 /* This file contains implementations of sorting algorithms.
  * You are NOT allowed to modify any of the given method headers.
  */
+import java.util.LinkedList;
 
-public class SortingAlgorithms {
+ public class SortingAlgorithms {
 
     /*
      * You may define additional helper functions here if you need to.
      * Make sure the helper functions have the private access modifier, as
      * they will only be used in this class.
      */
+    public Record[] recordList;
 
     public void insertionSort(Record[] arr, int n) {
-        // TODO: Implement this sorting algorithm here.
-	for (int i=1;i<n;i++)
-	{
-		Record temp = arr[i];
-		int j = i;
-		while((j>0)&&(array[j-1].getIdNumber>temp.getIdNumber))
-		{
-			array[j]=array[j-1];
-			j--;
-		}
-		array[j]=temp;
-	}
+        for (int i=1;i<n;i++)
+        {
+            Record temp = arr[i];
+            int j = i;
+            while((j>0)&&(arr[j-1].getIdNumber()>temp.getIdNumber()))
+            {
+                arr[j]=arr[j-1];
+                j--;
+            }
+            arr[j]=temp;
+        }
+
+        this.recordList = arr;
     }
 
     public void selectionSort(Record[] arr, int n) {
-        // TODO: Implement this sorting algorithm here.
-	for (int i=0;i<n;i++)
-	{
-		int key=i;
-		Record temp;
-		for (int j=i;j<n;j++)
-		{
-			if (arr[key].getIdNumber<arr[j].getIdNumber)
-			{
-				key=j;
-			}
-		}
-		temp=arr[key];
-		arr[key]=arr[i];
-		arr[i]=temp;
-	}
+        for (int i=0;i<n;i++)
+        {
+            int key=i;
+            Record temp;
+            for (int j=i;j<n;j++)
+            {
+                if (arr[key].getIdNumber()>arr[j].getIdNumber())
+                {
+                    key=j;
+                }
+            }
+            temp=arr[key];
+            arr[key]=arr[i];
+            arr[i]=temp;
+        }
+        this.recordList = arr;
     }
 
     public void mergeSort(Record[] arr, int p, int r) {
-        // TODO: Implement this sorting algorithm here.
+        LinkedList<Record[]> list = new LinkedList<Record[]>();
+        list.add(new Record[0]);
+        int arrayHalf = arr.length/2, leftIndex=0, rightIndex=0, mergeIndex=0;
+        if((arrayHalf*2)<arr.length) arrayHalf++; //if odd
 
+        Record[] leftSide = new Record[arrayHalf];
+        Record[] rightSide = new Record[arr.length/2];
+
+        for(int i=0; i<arrayHalf; i++)
+            leftSide[i] = arr[i];
+        for(int j=arrayHalf; j<arr.length; j++)
+            rightSide[j-arrayHalf] = arr[j];
+        
+        if(arr.length > 2) 
+        {   mergeSort(leftSide,0,0);
+            leftSide = this.recordList;
+            mergeSort(rightSide,0,0);
+            rightSide = this.recordList;
+        }
+        
+        while(leftIndex < arrayHalf && rightIndex < arr.length/2) 
+        {   if(leftSide[leftIndex].getIdNumber() < rightSide[rightIndex].getIdNumber())
+            {   arr[mergeIndex] = leftSide[leftIndex];
+                leftIndex++;
+            } else
+            {   arr[mergeIndex] = rightSide[rightIndex];
+                rightIndex++;
+            }
+            mergeIndex++;
+        }
+
+        if(leftIndex == arrayHalf)
+            System.arraycopy(rightSide, rightIndex, arr, mergeIndex, rightSide.length-rightIndex);
+        if(rightIndex == arr.length/2)
+            System.arraycopy(leftSide, leftIndex, arr, mergeIndex, leftSide.length-leftIndex);
+            
+        list.set(0,arr);
+        arr = list.get(0);
+        
+        this.recordList = arr;
     }
 
-    /*
-     * Define AT LEAST ONE more sorting algorithm here, apart from the
-     * ones given above. Make sure that the method accepts an array of
-     * records
-     */
+    public void bubbleSort(Record[] arr) {
+        Record temp = new Record("",0);
+        boolean flag = true;
+
+        for(int i=1; flag==true; i++)
+        {   flag = false;
+            for(int j=0; j<arr.length-i; j++)
+                if(arr[j].getIdNumber() > arr[j+1].getIdNumber())
+                {   temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    flag = true;
+                }
+        }
+
+        this.recordList = arr;
+    }
 
 }
