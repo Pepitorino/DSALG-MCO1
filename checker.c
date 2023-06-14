b8e89016af863f7a4efbb720de46953a0012beeb
Pepitorino <carlos.ambrosio2007@gmail.com>
2023-06-14 09:07:34 +0800

    Validator done

--- checker.c
+++ checker.c
@@ -1,26 +1,42 @@
 #include <stdio.h>
 #include <string.h>
+#include <dirent.h>
 
 int main()
 {
-	FILE* fp1 = fopen("totallyreversed.txtcheck.txt", "r");
-	FILE* fp2 = fopen("totallyreversed.txtcheck.txt", "r");
-	int skipper;
-	char title1[100], title2[100];
-	fscanf(fp1, "%s", title1);	
-	fscanf(fp2, "%s", title2);
-	fscanf(fp2, "%d", &skipper);
-	int i=0, first,second;
-	while(first!=second&&!feof(fp2))
+	DIR* folder = opendir("tests/");
+	struct dirent *entry;
+	entry=readdir(folder);
+	entry=readdir(folder);
+	
+	while(entry=readdir(folder))
 	{
-		fscanf(fp1, "%d", &first);
-		fscanf(fp2, "%d", &second);
-		if (second<first)
+		char name[100] = "tests/";
+		strcat(name, entry->d_name);
+		FILE* fp1 = fopen(name, "r");
+		FILE* fp2 = fopen(name, "r");
+		int skipper;
+		char title1[100], title2[100];
+		fscanf(fp1, "%s", title1);	
+		fscanf(fp2, "%s", title2);
+		fscanf(fp2, "%d", &skipper);
+		int i=0, first=0,second=1;
+		
+		while(first!=second&&!feof(fp2))
 		{
-			return 1;
+			fscanf(fp1, "%d", &first);
+			fscanf(fp2, "%d", &second);
+			if (second<first)
+			{
+				return 1;
+			}
+			i++;
 		}
-		printf("%d %d\n", first, second);
-		i++;
-	}	
+		
+		printf("%s PASSED\n", entry->d_name);
+		fclose(fp1);
+		fclose(fp2);
+	}
+	
 	return 0;	
 }
